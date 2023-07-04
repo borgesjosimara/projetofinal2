173480cb5aa4f71085b51850b18e97897cca4087
josimara <josimara.fborges@hotmail.com>
2023-07-04 14:27:07 -0300

    Iniciando um novo projeto

---
+++ projetofinalduo/src/main/java/br/com/senai/projetofinalduo/ProjetofinalduoApplication.java
@@ -0,0 +1,13 @@
+package br.com.senai.projetofinalduo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetofinalduoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetofinalduoApplication.class, args);
+	}
+
+}
---
+++ projetofinalduo/src/test/java/br/com/senai/projetofinalduo/ProjetofinalduoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senai.projetofinalduo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetofinalduoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
